e4561da1d372fb597f536d1328d023873228202b
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-16 14:36:13 -0600

    adjusted folders

--- server.js
+++ server.js
@@ -13,3 +13,4 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 // app.use('/api')
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
+// yes
