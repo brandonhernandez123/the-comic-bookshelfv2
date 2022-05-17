a95e991f1b584f66917f3432e473292e237ccb6a
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-16 23:42:07 -0600

    completed my comics routes

---
+++ routes/MyComicRoutes.js
@@ -0,0 +1,13 @@
+const Router = require('express').Router()
+
+const MyComics = require('../controllers/MyComics')
+
+Router.get('/mycomics', MyComics.getShelf)
+
+Router.get('./shelvedcomic/:comic_id', MyComics.getSingleComic)
+
+Router.delete('/deletecomic/:comic_id', MyComics.deleteComic)
+
+Router.post('/addcomic', MyComics.newComic)
+
+module.export = Router
