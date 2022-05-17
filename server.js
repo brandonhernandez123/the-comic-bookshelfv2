8c456770527fbba852ddab963acc5100ffc86804
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-16 23:43:25 -0600

    added mycomicroutes to serverjs

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
 
+const MyComicRoutes = require('./routes/MyComicRoutes')
+
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
@@ -11,6 +13,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-// app.use('/api')
+app.use('/api', MyComicRoutes)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
 // yes
