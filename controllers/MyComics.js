b1c1594a766483625ade54dd8a0888e18938dc71
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-16 22:46:42 -0600

    completed myComics controller

---
+++ controllers/MyComics.js
@@ -0,0 +1,48 @@
+const { myComics, Users } = require('../models')
+
+const getShelf = async (req, res) => {
+  try {
+    const comics = await myComics.findAll()
+    res.send(comics)
+  } catch (error) {
+    throw error
+  }
+}
+
+const getSingleComic = async (req, res) => {
+  try {
+    const comic = await myComics.findbyPk(req.params.comic_id)
+    res.send(comic)
+  } catch (error) {
+    throw error
+  }
+}
+
+const deleteComic = async (req, res) => {
+  try {
+    await myComics.destroy({ where: { id: req.params.comic_id } })
+    res.send({
+      msg: 'Comic has been removed from your shelf',
+      payload: req.params.comic_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+const newComic = async (req, res) => {
+  try {
+    const comic = await myComics.create(req.body)
+    res.send(comic)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  getShelf,
+  getSingleComic,
+  deleteComic,
+  newComic
+}
