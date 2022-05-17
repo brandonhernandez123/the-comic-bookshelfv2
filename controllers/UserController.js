55fec37c06d6d9ea49c17bb796fdbc81b881021a
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-16 22:32:04 -0600

    completed users controller for testing auth not attached yet

---
+++ controllers/UserController.js
@@ -0,0 +1,67 @@
+const { Users, myComics, reviewedComics } = require('../models')
+
+const GetUsers = async (req, res) => {
+  try {
+    const users = await Users.findAll()
+    res.send(users)
+  } catch (error) {
+    throw error
+  }
+}
+
+const NewUser = async (req, res) => {
+  try {
+    const newUser = await Users.create(req.body)
+    res.send(newUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+const singleUser = async (req, res) => {
+  try {
+    const oneUser = await Users.findbyPk(req.params.user_id, {
+      attributes: ['username', 'email'],
+      include: {
+        model: myComics,
+        model: reviewedComics
+      }
+    })
+    res.send(oneUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    await Users.destroy({ where: { id: req.params.user_id } })
+    res.send({
+      msg: 'Profile deleted successfully',
+      payload: req.params.user_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+const updateUser = async (req, res) => {
+  try {
+    const updateUser = await Users.update({
+      where: { id: req.params.user_id },
+      returning: true
+    })
+    res.send(updateUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetUsers,
+  NewUser,
+  singleUser,
+  deleteUser,
+  updateUser
+}
