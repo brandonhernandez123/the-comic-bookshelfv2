765016c18a52a1ff546b4a513b6e9b373ea21225
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-23 08:15:16 -0600

    added api key and added new dependency

--- client/src/globals.jsx
+++ client/src/globals.jsx
@@ -2,3 +2,5 @@ export const BASE_URL =
   process.env.NODE_ENV === 'production'
     ? `${window.location.origin}/api`
     : 'http://localhost:3001/api'
+
+    export const API_KEY = process.env.COMIC_VINE_API
