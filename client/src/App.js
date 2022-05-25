196122a33fb2082971980dd181cf5f22ec9d6c5f
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-25 08:21:51 -0600

    created comics page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Feed from './pages/Feed'
 import Characters from './pages/Characters'
+import Comics from './pages/Comics'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Feed} />
             <Route path="/characters" component={Characters} />
+            <Route path="/comics" component={Comics} />
           </Switch>
         </div>
       </Router>
---
+++ client/src/pages/Comics.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+
+const Comics = (props) => {
+    return(
+        <>
+        <h1>Hello World</h1>
+        </>
+    )
+}
+
+
+export default Comics;
