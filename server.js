56ddb32e224e3627a39e1b263013e11002fadd58
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-06-03 16:14:08 -0600

    deployment attempted fixes

--- server.js
+++ server.js
@@ -3,27 +3,24 @@ const cors = require('cors')
 const logger = require('morgan')
 const path = require('path')
 const express = require('express')
-const app = express()
+
 const MyComicRoutes = require('./routes/MyComicRoutes')
 const ReviewedComics = require('./routes/ReviewRoutes')
 const UserRoutes = require('./routes/UserRoutes')
 const AuthRoutes = require('./routes/AuthRoutes')
 
 const PORT = process.env.PORT || 3001
-
+const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use('/', express.static(path.join(__dirname, '/client/build')))
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', MyComicRoutes, ReviewedComics, UserRoutes, AuthRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
-  })
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+})
 
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
