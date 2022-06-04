437f1c12f676f036512e2766646d8bfd679e1384
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-06-03 19:55:35 -0600

    bug fixes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const logger = require('morgan')
 const path = require('path')
 const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const MyComicRoutes = require('./routes/MyComicRoutes')
 const ReviewedComics = require('./routes/ReviewRoutes')
@@ -11,13 +12,28 @@ const AuthRoutes = require('./routes/AuthRoutes')
 
 const PORT = process.env.PORT || 3001
 const app = express()
+
+const options = {
+  target: 'https://comicvine.gamespot.com/api/', // target host with the same base path
+  changeOrigin: true // needed for virtual hosted sites
+}
+
+const comicvineapiproxy = createProxyMiddleware(options)
+
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', express.static(path.join(__dirname, '/client/build')))
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/api', MyComicRoutes, ReviewedComics, UserRoutes, AuthRoutes)
+app.use(
+  '/api',
+  MyComicRoutes,
+  ReviewedComics,
+  UserRoutes,
+  AuthRoutes,
+  comicvineapiproxy
+)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
