ad432560e9afbb5d31e055ef21ec611855713e3c
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-06-03 19:38:34 -0600

    no changes

--- client/src/globals.jsx
+++ client/src/globals.jsx
@@ -7,10 +7,10 @@ export const BASE_URL =
 
 export const COMIC_VINE_API = process.env.REACT_APP_COMIC_VINE_API
 
-export const CHARACTER_COMIC_VINE = 'http://www.comicvine.com/api/characters?api_key='
+export const CHARACTER_COMIC_VINE = 'https://www.comicvine.com/api/characters?api_key='
 export const format = '&format=json'
-export const SingleCharacter = 'http://comicvine.gamespot.com/api/character/'
-export const SearchbyIssue = 'http://www.comicvine.com/api/issues?api_key='
+export const SingleCharacter = 'https://comicvine.gamespot.com/api/character/'
+export const SearchbyIssue = 'https://www.comicvine.com/api/issues?api_key='
 export const MARVEL_API = process.env.REACT_APP_MARVEL_API
 export const MARVEL_COMIC_SEARCH = 'https://gateway.marvel.com:443/v1/public/comics?dateRange='
 export const MARVEL_COMIC_SEARCH_NODATE = 'https://gateway.marvel.com:443/v1/public/comics?noVariants=true&title='
